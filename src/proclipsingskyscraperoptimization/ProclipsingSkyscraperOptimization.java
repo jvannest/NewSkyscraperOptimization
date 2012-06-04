a925ed7bbc8aabe538a44046b6f24b37f2862677
Jason Van Nest <jason.vannest@gmail.com>
2012-06-04 13:17:14 -0400

    1. Began GUI -- added four generic sliders.

--- src/proclipsingskyscraperoptimization/ProclipsingSkyscraperOptimization.java
+++ src/proclipsingskyscraperoptimization/ProclipsingSkyscraperOptimization.java
@@ -8,36 +8,69 @@ package proclipsingskyscraperoptimization;
 import processing.core.PApplet;
 import proclipsingskyscraperoptimization.Skyscraper;
 import peasy.*;
-
+import controlP5.*;
 
 public class ProclipsingSkyscraperOptimization extends PApplet {
 	
 	PeasyCam cam;
+	ControlP5 controlP5;
+	//PMatrix3D currCameraMatrix;
+	//PGraphics3D g3; 
 	
 	Skyscraper mySkyscraper;
 	int numLevels = 15;
 	
 	int feet = 12;
+	
+	int guiWidth = 350;
+	int guiHeight = 900;
+
 
 	public void setup() {
 		size(1200,900, P3D);
-		//PFont font = loadFont("FFScala-32.vlw");
-		//textFont(font, 14);
 		
 		cam = new PeasyCam(this, 200*feet);
 		cam.setMinimumDistance(2*feet);
 		cam.setMaximumDistance(2000*feet);
+		
+		controlP5 = new ControlP5(this);
+		//PFont p = font = loadFont("Helvetica-15.vlw");
+		//controlP5.setControlFont(p);
 		  
 		mySkyscraper = new Skyscraper(numLevels, this);
+		
+		setupSliders();
 	}
 
 	public void draw() {
 		background(250);
 		prePeasy();
 		mySkyscraper.draw();
+		
+		gui();
 	}
 	
 	void prePeasy(){
 		translate(0, 0, -2500);
 	}
+
+	void gui() {
+		noSmooth();
+		//currCameraMatrix = new PMatrix3D(g3.camera);
+		camera();
+		fill(155);
+		noStroke();
+		//rect(0, 0, guiWidth, guiHeight);
+		controlP5.draw();
+		//g3.camera = currCameraMatrix;
+	}
+	
+	void setupSliders() {
+		  //controlP5.addSlider("slider",0,200,128,20,100,10,100);
+		  controlP5.addSlider("VAR 1", 0, 3, 2, 20, 50, 200, 20);
+		  controlP5.addSlider("VAR 2", 0, 3, 2, 20, 75, 200, 20);
+		  controlP5.addSlider("VAR 3", 0, 4, 3, 20, 100, 200, 20);
+		  controlP5.addSlider("VAR 4", 0, 300, 100, 20, 125, 200, 20);
+		}
+
 }
